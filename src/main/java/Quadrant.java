560ed2620651e629dec21a1d69c79fcf80527ab0
RF1984 <rafaeldcfernandes@gmail.com>
2019-11-24 22:27:11 +0100

    quandrant solution

---
+++ src/main/java/Quadrant.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Quadrant {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int y = in.nextInt();
+        if (x <= 1000 && x > 0 && y > 0 && y <= 1000) {
+            System.out.println("1");
+        }
+        else if (x >= -1000 && x < 0 && y > 0 && y <= 1000) {
+            System.out.println("2");
+        }
+        else if (x >= -1000 && x < 0 && y >= -1000 && y < 0) {
+            System.out.println("3");
+        }
+        else if(x <= 1000 && x > 0 && y >= -1000 & y < 0) {
+            System.out.println("4");
+        }
+    }
+}
