4c18f3f360c8fd7fed54a2a4c391e9591571bf91
RF1984 <rafaeldcfernandes@gmail.com>
2019-12-01 00:18:34 +0100

    FizzBuzz exercise

--- src/main/java/Carrots.java
+++ src/main/java/Carrots.java
@@ -3,6 +3,14 @@ import java.util.Scanner;
 public class Carrots {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        String input = in.nextLine();
+        String [] inputList = input.split(" ");
+        int contest = Integer.parseInt(inputList[0]);
+        int problems = Integer.parseInt(inputList[1]);
+        for (int i=1; i<= contest; i++) {
+            Scanner in2 = new Scanner(System.in);
+            String input2 = in.nextLine();
+        }
+        System.out.println(problems);
     }
 }
---
+++ src/main/java/FizzBuzz.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        String[] inputList = input.split(" ");
+        int x = Integer.parseInt(inputList[0]);
+        int y = Integer.parseInt(inputList[1]);
+        int n = Integer.parseInt(inputList[2]);
+
+        for (int i=1; i<= n; i++) {
+            if (i % y == 0 && i % x == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else if (i % x == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % y == 0) {
+                System.out.println("Buzz");
+            }
+
+            else System.out.println(i);
+        }
+
+    }
+}
